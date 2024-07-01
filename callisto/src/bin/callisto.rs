676d6e9f397fd444e9726ef6d592a43adc78d7db
A K <alex.kesling@gmail.com>
2024-07-01 15:00:04 -0400

    Switch CLI to a command + arg structure instead of everything being root flags

--- callisto/src/bin/callisto.rs
+++ callisto/src/bin/callisto.rs
@@ -5,20 +5,27 @@ use serde::Serialize;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None, arg_required_else_help = true)]
 struct Args {
-    /// Command to execute
-    #[arg(long, short)]
-    command: String,
+    #[command(subcommand)]
+    command: Command,
+}
 
-    /// Command to execute
-    #[arg(long, short, default_value_t, value_enum)]
-    engine: Engine,
+#[derive(clap::Subcommand, Debug)]
+enum Command {
+    Exec {
+        /// Command to execute
+        command: String,
+
+        /// Command to execute
+        #[arg(long, short, default_value_t, value_enum)]
+        engine: Engine,
+    },
 }
 
 #[derive(clap::ValueEnum, Clone, Debug, Serialize, Default)]
 enum Engine {
-    #[default]
     Polars,
     DuckDB,
+    #[default]
     DataFusion,
 }
 
@@ -38,22 +45,30 @@ async fn main() -> anyhow::Result<()> {
 
     let args = Args::parse();
 
-    println!(
-        "Running command '{}' on engine '{}'",
-        args.command,
-        &serde_json::to_string(&args.engine).unwrap()
-    );
+    match args.command {
+        Command::Exec {
+            command,
+            engine: engine_type,
+        } => {
+            println!(
+                "Running command '{}' on engine '{}'",
+                command,
+                &serde_json::to_string(&engine_type).unwrap()
+            );
 
-    let mut engine = args.engine.new()?;
-    let executions = engine.execute(&args.command).await?;
-    for (statement, mut stream) in executions {
-        println!("\n$ {}", statement.to_string());
-        let mut batches = Vec::new();
-        while let Some(items) = stream.next().await {
-            batches.push(items?);
+            let mut engine = engine_type.new()?;
+            let executions = engine.execute(&command).await?;
+            for (statement, mut stream) in executions {
+                println!("\n$ {}", statement.to_string());
+                let mut batches = Vec::new();
+                while let Some(items) = stream.next().await {
+                    batches.push(items?);
+                }
+                let pretty_results =
+                    arrow::util::pretty::pretty_format_batches(&batches)?.to_string();
+                println!("Results:\n{}", pretty_results);
+            }
+            Ok(())
         }
-        let pretty_results = arrow::util::pretty::pretty_format_batches(&batches)?.to_string();
-        println!("Results:\n{}", pretty_results);
     }
-    Ok(())
 }
