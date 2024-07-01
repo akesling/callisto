abf48f0f755f6ae979dafd5108f4c2f6e016bb62
A K <alex.kesling@gmail.com>
2024-07-01 15:28:39 -0400

    Factor repl out to helper

--- callisto/src/bin/callisto.rs
+++ callisto/src/bin/callisto.rs
@@ -12,6 +12,7 @@ struct Args {
 #[derive(clap::Subcommand, Debug)]
 enum Command {
     Exec {
+        // TODO(akesling): Add output format control
         /// Command to execute
         command: String,
 
@@ -47,9 +48,6 @@ impl Engine {
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     use futures::stream::StreamExt as _;
-    use std::io::Write as _;
-    use tokio::io::AsyncBufReadExt as _;
-
     let args = Args::parse();
 
     match args.command {
@@ -82,39 +80,7 @@ async fn main() -> anyhow::Result<()> {
         } => {
             let mut engine = engine_type.new()?;
 
-            let stdin = tokio::io::stdin();
-            let reader = tokio::io::BufReader::new(stdin);
-            let mut lines = reader.lines();
-
-            while let Some(line) = {
-                print!("> ");
-                std::io::stdout().flush()?;
-                lines.next_line().await.unwrap()
-            } {
-                let command = line.trim();
-                if ["exit", "bye", "q", "quit"].contains(&command.to_lowercase().as_str()) {
-                    println!("Goodbye!");
-                    break;
-                }
-
-                let executions = match engine.execute(&command).await {
-                    Ok(e) => e,
-                    Err(error) => {
-                        println!("Error: {:?}", error);
-                        continue;
-                    }
-                };
-                for (statement, mut stream) in executions {
-                    println!("\n$ {}", statement.to_string());
-                    let mut batches = Vec::new();
-                    while let Some(items) = stream.next().await {
-                        batches.push(items?);
-                    }
-                    let pretty_results =
-                        arrow::util::pretty::pretty_format_batches(&batches)?.to_string();
-                    println!("Results:\n{}", pretty_results);
-                }
-            }
+            callisto::Repl::run(&mut engine).await?;
             Ok(())
         }
     }
--- callisto/src/lib.rs
+++ callisto/src/lib.rs
@@ -1,1 +1,46 @@
 pub use callisto_engines::{Engine, EngineInterface};
+
+pub struct Repl {}
+
+impl Repl {
+    pub async fn run(engine: &mut Box<dyn EngineInterface>) -> anyhow::Result<()> {
+        use futures::stream::StreamExt as _;
+        use std::io::Write as _;
+        use tokio::io::AsyncBufReadExt as _;
+
+        let stdin = tokio::io::stdin();
+        let reader = tokio::io::BufReader::new(stdin);
+        let mut lines = reader.lines();
+
+        while let Some(line) = {
+            print!("> ");
+            std::io::stdout().flush()?;
+            lines.next_line().await.unwrap()
+        } {
+            let command = line.trim();
+            if ["exit", "bye", "q", "quit"].contains(&command.to_lowercase().as_str()) {
+                break;
+            }
+
+            let executions = match engine.execute(&command).await {
+                Ok(e) => e,
+                Err(error) => {
+                    println!("Error: {:?}", error);
+                    continue;
+                }
+            };
+            for (statement, mut stream) in executions {
+                println!("\n$ {}", statement.to_string());
+                let mut batches = Vec::new();
+                while let Some(items) = stream.next().await {
+                    batches.push(items?);
+                }
+                let pretty_results =
+                    arrow::util::pretty::pretty_format_batches(&batches)?.to_string();
+                println!("Results:\n{}", pretty_results);
+            }
+        }
+        println!("\nGoodbye!");
+        Ok(())
+    }
+}
